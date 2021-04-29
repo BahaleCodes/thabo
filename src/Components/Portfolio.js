5a783b16cd5149a0943333d412af0d7b10e249e1
BahaleCodes <74527555+BahaleCodes@users.noreply.github.com>
2021-04-29 12:09:55 +0200

    New Profile pic

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,7 +13,7 @@ class Portfolio extends Component {
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
-            <a href="https://www.taahirahmodelingacademy.co.za/tma" style={{ textAlign: "center" }}>{projects.title}</a>
+            <a href={projects.url} style={{ textAlign: "center" }}>{projects.title}</a>
           </div>
         </div>
       );
